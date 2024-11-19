f900d0422465923284b4b2c0b65f472d06ec9f03
Danilo Pianini <danilo.pianini@unibo.it>
2024-11-19 17:15:29 +0100

    fix: add @Nonnull annotations

--- src/main/java/de/aaschmid/gradle/plugins/cpd/internal/Reports.java
+++ src/main/java/de/aaschmid/gradle/plugins/cpd/internal/Reports.java
@@ -15,6 +15,7 @@ import org.gradle.api.reporting.Report;
 import org.gradle.api.reporting.ReportContainer;
 import org.gradle.api.specs.Spec;
 
+import javax.annotation.Nonnull;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -33,6 +34,7 @@ public class Reports<T extends Report> implements ReportContainer<T> {
         this.enabled = this.reports.matching(report -> report.getRequired().get());
     }
 
+    @Nonnull
     @Override
     public NamedDomainObjectSet<T> getEnabled() {
         return this.enabled;
@@ -43,22 +45,22 @@ public class Reports<T extends Report> implements ReportContainer<T> {
     }
 
     @Override
-    public boolean add(final T report) {
+    public boolean add(@Nonnull final T report) {
         throw new ImmutableViolationException();
     }
 
     @Override
-    public boolean addAll(final Collection<? extends T> reps) {
+    public boolean addAll(@Nonnull final Collection<? extends T> reps) {
         throw new ImmutableViolationException();
     }
 
     @Override
-    public void addLater(final Provider<? extends T> provider) {
+    public void addLater(@Nonnull final Provider<? extends T> provider) {
         throw new ImmutableViolationException();
     }
 
     @Override
-    public void addAllLater(final Provider<? extends Iterable<T>> provider) {
+    public void addAllLater(@Nonnull final Provider<? extends Iterable<T>> provider) {
         throw new ImmutableViolationException();
     }
 
@@ -68,12 +70,12 @@ public class Reports<T extends Report> implements ReportContainer<T> {
     }
 
     @Override
-    public boolean removeAll( final Collection<?> reps) {
+    public boolean removeAll(@Nonnull final Collection<?> reps) {
         throw new ImmutableViolationException();
     }
 
     @Override
-    public boolean retainAll( final Collection<?> reps) {
+    public boolean retainAll(@Nonnull final Collection<?> reps) {
         throw new ImmutableViolationException();
     }
 
@@ -83,65 +85,76 @@ public class Reports<T extends Report> implements ReportContainer<T> {
     }
 
     @Override
-    public boolean containsAll( final Collection<?> reps) {
+    public boolean containsAll(@Nonnull final Collection<?> reps) {
         return this.reports.containsAll(reps);
     }
 
+    @Nonnull
     @Override
     public Namer<T> getNamer() {
         return Report::getName;
     }
 
+    @Nonnull
     @Override
     public SortedMap<String, T> getAsMap() {
         return this.reports.getAsMap();
     }
 
+    @Nonnull
     @Override
     public SortedSet<String> getNames() {
         return this.reports.getNames();
     }
 
     @Override
-    public T findByName(final String name) {
+    public T findByName(@Nonnull final String name) {
         return this.reports.findByName(name);
     }
 
+    @Nonnull
     @Override
-    public T getByName(final String name) throws UnknownDomainObjectException {
+    public T getByName(@Nonnull final String name) throws UnknownDomainObjectException {
         return this.reports.getByName(name);
     }
 
+    @Nonnull
     @Override
-    public T getByName(final String name, final Closure configureClosure) throws UnknownDomainObjectException {
+    public T getByName(@Nonnull final String name, @Nonnull final Closure configureClosure) throws UnknownDomainObjectException {
         return this.reports.getByName(name, configureClosure);
     }
 
+    @Nonnull
     @Override
-    public T getByName(final String name, final Action<? super T> configureAction) throws UnknownDomainObjectException {
+    public T getByName(@Nonnull final String name, @Nonnull final Action<? super T> configureAction) throws UnknownDomainObjectException {
         return this.reports.getByName(name, configureAction);
     }
 
+    @Nonnull
     @Override
-    public T getAt(final String name) throws UnknownDomainObjectException {
+    public T getAt(@Nonnull final String name) throws UnknownDomainObjectException {
         return this.reports.getAt(name);
     }
 
+    @Nonnull
     @Override
-    public Rule addRule(final Rule rule) {
+    public Rule addRule(@Nonnull final Rule rule) {
         return this.reports.addRule(rule);
     }
 
+    @Nonnull
     @Override
-    public Rule addRule(final String description, final Closure ruleAction) {
+    public Rule addRule(@Nonnull final String description, @Nonnull final Closure ruleAction) {
         return this.reports.addRule(description, ruleAction);
     }
 
+    @Nonnull
     @Override
-    public Rule addRule(final String description, final Action<String> ruleAction) {
+    public Rule addRule(@Nonnull final String description, @Nonnull final Action<String> ruleAction) {
         return this.reports.addRule(description, ruleAction);
     }
 
+    @Nonnull
     @Override
     public List<Rule> getRules() {
         return this.reports.getRules();
@@ -162,119 +175,137 @@ public class Reports<T extends Report> implements ReportContainer<T> {
         return this.reports.contains(report);
     }
 
+    @Nonnull
     @Override
     public Iterator<T> iterator() {
         return this.reports.iterator();
     }
 
+    @Nonnull
     @Override
     public Object[] toArray() {
         return this.reports.toArray();
     }
 
+    @Nonnull
     @Override
     @SuppressWarnings("unchecked")
-    public <T1> T1[] toArray( final T1[] arr) {
+    public <T1> T1[] toArray(@Nonnull final T1[] arr) {
         return (T1[])this.reports.toArray((T[])arr);
     }
 
+    @Nonnull
     @Override
     public Map<String, T> getEnabledReports() {
         return this.enabled.getAsMap();
     }
 
+    @Nonnull
     @Override
-    public <S extends T> NamedDomainObjectSet<S> withType(final Class<S> type) {
+    public <S extends T> NamedDomainObjectSet<S> withType(@Nonnull final Class<S> type) {
         return this.reports.withType(type);
     }
 
+    @Nonnull
     @Override
-    public <S extends T> DomainObjectCollection<S> withType(final Class<S> type, final Action<? super S> configureAction) {
+    public <S extends T> DomainObjectCollection<S> withType(@Nonnull final Class<S> type, @Nonnull final Action<? super S> configureAction) {
         return this.reports.withType(type, configureAction);
     }
 
+    @Nonnull
     @Override
-    public <S extends T> DomainObjectCollection<S> withType(final Class<S> type, final Closure configureClosure) {
+    public <S extends T> DomainObjectCollection<S> withType(@Nonnull final Class<S> type, @Nonnull final Closure configureClosure) {
         return this.reports.withType(type, configureClosure);
     }
 
+    @Nonnull
     @Override
-    public NamedDomainObjectSet<T> matching(final Spec<? super T> spec) {
+    public NamedDomainObjectSet<T> matching(@Nonnull final Spec<? super T> spec) {
         return this.reports.matching(spec);
     }
 
+    @Nonnull
     @Override
-    public NamedDomainObjectSet<T> matching(final Closure spec) {
+    public NamedDomainObjectSet<T> matching(@Nonnull final Closure spec) {
         return this.reports.matching(spec);
     }
 
+    @Nonnull
     @Override
-    public Action<? super T> whenObjectAdded(final Action<? super T> action) {
+    public Action<? super T> whenObjectAdded(@Nonnull final Action<? super T> action) {
         return this.reports.whenObjectAdded(action);
     }
 
     @Override
-    public void whenObjectAdded(final Closure action) {
+    public void whenObjectAdded(@Nonnull final Closure action) {
         this.reports.whenObjectAdded(action);
     }
 
+    @Nonnull
     @Override
-    public Action<? super T> whenObjectRemoved(final Action<? super T> action) {
+    public Action<? super T> whenObjectRemoved(@Nonnull final Action<? super T> action) {
         return this.reports.whenObjectRemoved(action);
     }
 
     @Override
-    public void whenObjectRemoved(final Closure action) {
+    public void whenObjectRemoved(@Nonnull final Closure action) {
         this.reports.whenObjectRemoved(action);
     }
 
     @Override
-    public void all(final Action<? super T> action) {
+    public void all(@Nonnull final Action<? super T> action) {
         this.reports.all(action);
     }
 
     @Override
-    public void all(final Closure action) {
+    public void all(@Nonnull final Closure action) {
         this.reports.all(action);
     }
 
     @Override
-    public void configureEach(final Action<? super T> action) {
+    public void configureEach(@Nonnull final Action<? super T> action) {
         this.reports.configureEach(action);
     }
 
+    @Nonnull
     @Override
-    public NamedDomainObjectSet<T> named(Spec<String> nameFilter) {
+    public NamedDomainObjectSet<T> named(@Nonnull Spec<String> nameFilter) {
         return this.reports.named(nameFilter);
     }
 
+    @Nonnull
     @Override
-    public NamedDomainObjectProvider<T> named(final String name) throws UnknownDomainObjectException {
+    public NamedDomainObjectProvider<T> named(@Nonnull final String name) throws UnknownDomainObjectException {
         return this.reports.named(name);
     }
 
+    @Nonnull
     @Override
-    public NamedDomainObjectProvider<T> named(final String name, final Action<? super T> configurationAction) throws UnknownDomainObjectException {
+    public NamedDomainObjectProvider<T> named(@Nonnull final String name, @Nonnull final Action<? super T> configurationAction) throws UnknownDomainObjectException {
         return this.reports.named(name, configurationAction);
     }
 
+    @Nonnull
     @Override
-    public <S extends T> NamedDomainObjectProvider<S> named(final String name, final Class<S> type) throws UnknownDomainObjectException {
+    public <S extends T> NamedDomainObjectProvider<S> named(@Nonnull final String name, @Nonnull final Class<S> type) throws UnknownDomainObjectException {
         return this.reports.named(name, type);
     }
 
+    @Nonnull
     @Override
-    public <S extends T> NamedDomainObjectProvider<S> named(final String name, final Class<S> type, final Action<? super S> configurationAction) throws UnknownDomainObjectException {
+    public <S extends T> NamedDomainObjectProvider<S> named(@Nonnull final String name, @Nonnull final Class<S> type, @Nonnull final Action<? super S> configurationAction) throws UnknownDomainObjectException {
         return this.reports.named(name, type, configurationAction);
     }
 
+    @Nonnull
     @Override
     public NamedDomainObjectCollectionSchema getCollectionSchema() {
         return this.reports.getCollectionSchema();
     }
 
+    @Nonnull
     @Override
-    public Set<T> findAll(final Closure spec) {
+    public Set<T> findAll(@Nonnull final Closure spec) {
         return this.reports.findAll(spec);
     }
 
